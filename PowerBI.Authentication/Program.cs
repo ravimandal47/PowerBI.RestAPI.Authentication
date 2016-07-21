7a91d6dcc8ea1db9ca71910f1d8ed2ba86c3da7e
Ravi Mandal <ravimandal47@hotmail.com>
2016-07-22 00:59:08 +0545

    Initial Commit

--- PowerBI.Authentication/Program.cs
+++ PowerBI.Authentication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,57 @@ namespace PowerBI.Authentication
 {
     class Program
     {
+        //TODO - Replace {client id} with your client app ID. 
+        //Format : e2xxxxx-e4xxx-44xx-9bxx-xxxxxxxxx
+        private static string clientID = "{client-id}";
+
+        //RedirectUri you used when you registered your app.
+        //For a client app, a redirect uri gives AAD more details on the specific application that it will authenticate.
+        private static string redirectUri = "https://login.live.com/oauth20_desktop.srf";
+
+        //Resource Uri for Power BI API
+        private static string resourceUri = "https://analysis.windows.net/powerbi/api";
+
+        //OAuth2 authority Uri
+        private static string authority = "https://login.windows.net/common/oauth2/authorize";
+
+        private static AuthenticationContext authContext = null;
+        private static string token = String.Empty;
         static void Main(string[] args)
         {
+            Task.Run(async () =>
+            {
+                string token = await AccessToken();
+                Console.WriteLine(string.Format("Power BI Token :{0}",token));
+                Console.ReadLine();
+            }).Wait();
+        }
+        /// <summary>
+        /// Use AuthenticationContext to get an access token
+        /// </summary>
+        /// <returns></returns>
+        static async Task<string> AccessToken()
+        {
+            if (token == String.Empty)
+            {
+                //Get Azure access token
+                // Create an instance of TokenCache to cache the access token
+                TokenCache TC = new TokenCache();
+                // Create an instance of AuthenticationContext to acquire an Azure access token
+                authContext = new AuthenticationContext(authority, TC);
+                // Call AcquireToken to get an Azure token from Azure Active Directory token issuance endpoint
+                var authenticationResult = await authContext.AcquireTokenAsync(resourceUri, clientID, new Uri(redirectUri), new PlatformParameters(PromptBehavior.RefreshSession));
+                token = authenticationResult.AccessToken;
+            }
+            else
+            {
+                //Get the token in the cache
+                var authenticationResult = await authContext.AcquireTokenSilentAsync(resourceUri, clientID);
+                token = authenticationResult.AccessToken;
+
+            }
+
+            return token;
         }
     }
 }
